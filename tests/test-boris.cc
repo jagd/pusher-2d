f24419576f0ab19c1658e9ef520d18462e53500e
Wu <me@crwu.de>
2018-08-10 15:28:38 +0200

    Add test for Boris small orbit

--- tests/test-boris.cc
+++ tests/test-boris.cc
@@ -72,12 +72,37 @@ TEST(Boris, PlainLargeOrbit) {
     const auto mf = std::make_shared<HomogeneousMagField>(B);
     auto pusher = BorisPusher(ef, mf);
 
-    double dt = 1e-13;
-    double startAngle = dt * omega/2;
+    const double dt = 1e-13;
+    const double startAngle = dt * omega/2;
     pusher.setElectronInfo(r*std::cos(startAngle), r*std::sin(startAngle),0, 0, u, 0);
     for (int i = 0; i < 10; ++i) {
         pusher.step(dt);
         const auto p = pusher.pos();
         ASSERT_NEAR(r, std::sqrt(p.x*p.x+p.y*p.y), r*1e-6);
+    }
+}
+
+
+TEST(Boris, PlainSmallOrbit) {
+    const double B = 1.0; // f ~ 28 GHz
+    const double u = 1e8; // f ~ 26 GHz considering gamma
+    const double rLarmor = -M0/Q0/B * u; // ~ 0.5 mm
+    const double gamma = u2gamma(u);
+    const double v = u/gamma;
+    const double omega = v/rLarmor;
+
+    const auto ef = std::make_shared<ZeroEField>();
+    const auto mf = std::make_shared<HomogeneousMagField>(B);
+    auto pusher = BorisPusher(ef, mf);
+
+    const double dt = 1e-13;
+    const double startAngle = dt * omega/2;
+    const double offset = 2*rLarmor;
+    pusher.setElectronInfo(offset+rLarmor*std::cos(startAngle), rLarmor*std::sin(startAngle),0, 0, u, 0);
+    for (int i = 0; i < 10; ++i) {
+        pusher.step(dt);
+        const auto p = pusher.pos();
+        const double rSoll = std::sqrt(offset*offset +rLarmor*rLarmor + 2*offset*rLarmor*std::cos(startAngle+omega*dt*(i+1)));
+        ASSERT_NEAR(rSoll, std::sqrt(p.x*p.x+p.y*p.y), rLarmor*1e-6);
     }
 }
