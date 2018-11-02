8a417d84a8cddc4fd217f56bd0ae7385df6c4b79
Chuanren Wu <wuchuanren@gmail.com>
2018-11-02 16:20:25 +0100

    Update benchmark: planar-small-orbit

--- benchmark/planar-smallorbit.cc
+++ benchmark/planar-smallorbit.cc
@@ -15,56 +15,97 @@ int main()
     const auto mf = std::make_shared<ConstBzField>(B);
     auto aphi = APhiPusher(ef, mf);
     auto boris = BorisPusher(ef, mf);
+    auto lf = LeapFrogPusher(ef, mf);
+    auto rk = RK4Pusher(ef, mf);
 
     const double offset = 2*rLarmor;
 
 #ifdef DEMO
-    std::cout << "# 1:omega*dt 2:turns 3:error_r_boris 4:error_r_aphi\n";
+    std::cout << "# 1:omega*dt 2:turns 3:rel_error_r_leapfrog 4:rel_error_r_boris 5:rel_error_r_aphi 6:rel_error_r_rk4\n";
 #else
-    std::cout << "# 1:omega*dt 2:tol_boris 3:tol_aphi\n";
+    std::cout << "# 1:omega*dt 2:tol_r_leapfrog 3:tol_r_boris 4:tol_r_phi 5:tol_r_rk4 "
+                 "6:tol_gamma_leapfrog  7:tol_gamma_boris 8:tol:gamma_aphi 9:tol_gamma_rk4\n";
 #endif
 
-    for (double dt = 1e-12; dt > 1e-16; dt *= 0.5) {
+    for (double dt = 1e-12; dt > 1e-18; dt *= 0.5) {
         const int64_t steps = static_cast<int64_t>(std::ceil(100*2*M_PI / (omega*dt)));
         std::clog << "omega*dt: " << omega*dt << '\n';
         const double startAngle = dt * omega/2;
         aphi.setElectronInfo(0, std::sqrt(offset*offset +rLarmor*rLarmor + 2*offset*rLarmor*std::cos(startAngle)), 0, 0, aphi.pTheta(0, offset+rLarmor, u), gamma);
         boris.setElectronInfo(offset+rLarmor*std::cos(startAngle), rLarmor*std::sin(startAngle),0, 0, u, 0);
+        lf.setElectronInfo(offset+rLarmor*std::cos(startAngle), rLarmor*std::sin(startAngle),0, 0, u, 0);
+        rk.setElectronInfo(offset+rLarmor*std::cos(0), rLarmor*std::sin(0),0, 0, u, 0);
+        // in order to start from the same position
+        for (int i = 0; i < 1024; ++i) {
+            rk.step(dt / 2048);
+        }
 #ifdef DEMO
+        if (dt < 1e-15)
+            break;
         int64_t trigger = 1000;
         for (int64_t i = 1; i <= steps; ++i) {
+            ++trigger;
             if (trigger*omega*dt > M_PI/60) { // every ~3 degree
+                trigger = 0;
                 const auto pa = aphi.pos();
                 const auto pb = boris.pos();
+                const auto pl = lf.pos();
+                const auto pr = rk.pos();
                 // omega*dt*i, i start from 1 instead of 0,
                 // because the first step was considered in the intialization.
                 const double rSoll = std::sqrt(offset*offset +rLarmor*rLarmor + 2*offset*rLarmor*std::cos(omega*dt*i+startAngle));
                 std::cout << omega*dt << ' '
                           << omega*dt*i/(2*M_PI) << ' '
-                          <<  (fromPV3D(pb).r-rSoll)/rSoll << ' '
-                          <<  (pa.r-rSoll)/rSoll << '\n';
-                trigger = 0;
+                          << (fromPV3D(pl).r - rSoll) / rSoll << ' '
+                          << (fromPV3D(pb).r-rSoll)/rSoll << ' '
+                          << (pa.r-rSoll)/rSoll << ' '
+                          << (fromPV3D(pr).r - rSoll) / rSoll << '\n';
             }
             aphi.step(dt);
             boris.step(dt);
-            ++trigger;
+            lf.step(dt);
+            rk.step(dt);
         }
         std::cout << '\n';
 #else
-        double tola = 0;
-        double tolb = 0;
+        double tolA = 0;
+        double tolB = 0;
+        double tolL = 0;
+        double tolR = 0;
+        double tolGL = 0;
+        double tolGB = 0;
+        double tolGA = 0;
+        double tolGR = 0;
         for (int64_t i = 1; i <= steps; ++i) {
             aphi.step(dt);
             boris.step(dt);
+            lf.step(dt);
+            rk.step(dt);
             // omega*dt*i, i start from 1 instead of 0,
             // because the first step was considered in the intialization.
             const double rSoll = std::sqrt(offset*offset +rLarmor*rLarmor + 2*offset*rLarmor*std::cos(omega*dt*i+startAngle));
             const auto pa = aphi.pos();
             const auto pb = boris.pos();
-            tola = std::max(tola, std::abs((pa.r-rSoll)/rSoll));
-            tolb = std::max(tolb, std::abs((fromPV3D(pb).r-rSoll)/rSoll));
+            const auto pl = lf.pos();
+            const auto pr = rk.pos();
+            tolA = std::max(tolA, std::abs((pa.r-rSoll)/rSoll));
+            tolB = std::max(tolB, std::abs((fromPV3D(pb).r-rSoll)/rSoll));
+            tolL = std::max(tolL, std::abs((fromPV3D(pl).r-rSoll)/rSoll));
+            tolR = std::max(tolR, std::abs((fromPV3D(pr).r-rSoll)/rSoll));
+            tolGL = std::max(tolGL, std::abs(lf.gammaLastHalf() - gamma));
+            tolGB = std::max(tolGB, std::abs(boris.gammaCurrent() - gamma));
+            tolGA = std::max(tolGA, std::abs(aphi.gammaCurrent() - gamma));
+            tolGR = std::max(tolGR, std::abs(rk.gammaCurrent() - gamma));
         }
-        std::cout << omega*dt << ' ' << tolb << ' ' << tola << '\n';
+        std::cout << omega*dt << ' '
+                  << tolL << ' '
+                  << tolB << ' '
+                  << tolA << ' '
+                  << tolR << ' '
+                  << tolGL << ' '
+                  << tolGB << ' '
+                  << tolGA << ' '
+                  << tolGR << '\n';
 #endif
     }
     return 0;
