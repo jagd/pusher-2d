2742afcb36876a5a6a2140baafaba986ecfee4d5
Chuanren Wu <contact@thedrwu.com>
2018-11-07 23:14:45 +0100

    Do a performance benchmark with zero fields

--- benchmark/perf-zero.cc
+++ benchmark/perf-zero.cc
@@ -2,20 +2,54 @@
 #include <iostream>
 #include <chrono>
 
+class ZeroField : public IStaticMagField, public IStaticEField
+{
+    // Inherited via IStaticMagField
+    virtual double aTheta(double z, double r) const override
+    {
+        return 0.0;
+    }
+    virtual double aTheta2z(double z, double r) const override
+    {
+        return 0.0;
+    }
+    virtual double aTheta2r(double z, double r) const override
+    {
+        return 0.0;
+    }
+    virtual double br(double z, double r) const override
+    {
+        return 0.0;
+    }
+    virtual double bz(double z, double r) const override
+    {
+        return 0.0;
+    }
+
+    // Inherited via IStaticEField
+    virtual double ez(double z, double r) const override
+    {
+        return 0.0;
+    }
+    virtual double er(double z, double r) const override
+    {
+        return 0.0;
+    }
+};
+
+
 #if defined(APHI_ONLY) || defined(BORIS_ONLY) || defined(LEAPFROG_ONLY) || defined(RK4_ONLY) 
 #define SINGLE_PUSHER
 #endif
 
 int main()
 {
-    const auto ef = std::make_shared<ZeroEField>();
-    const auto mf = std::make_shared<ConstBzField>(0);
-
-    const int64_t steps = 20000000;
+    const auto zf = std::make_shared<ZeroField>();
+    const int64_t steps = 200000000;
     const double dt = 1e-15;
 
 #if !defined(SINGLE_PUSHER) || defined(LEAPFROG_ONLY)
-    auto lf = LeapFrogPusher(ef, mf);
+    auto lf = LeapFrogPusher(zf, zf);
     lf.setElectronInfo(1, 0, 0, 0, 0, 0);
     const auto lfBegin = std::chrono::steady_clock::now();
     for (int64_t i = 0; i < steps; ++i) {
@@ -26,7 +60,7 @@ int main()
 #endif
 
 #if !defined(SINGLE_PUSHER) || defined(BORIS_ONLY)
-    auto boris = BorisPusher(ef, mf);
+    auto boris = BorisPusher(zf, zf);
     boris.setElectronInfo(1, 0, 0, 0, 0, 0);
     const auto borisBegin = std::chrono::steady_clock::now();
     for (int64_t i = 0; i < steps; ++i) {
@@ -37,7 +71,7 @@ int main()
 #endif
 
 #if !defined(SINGLE_PUSHER) || defined(APHI_ONLY)
-    auto aphi = APhiPusher(ef, mf);
+    auto aphi = APhiPusher(zf, zf);
     aphi.setElectronInfo(0, 1, 0, 0, aphi.pTheta(0, 1, 0), 1);
     const auto aphiBegin = std::chrono::steady_clock::now();
     for (int64_t i = 0; i < steps; ++i) {
@@ -48,7 +82,7 @@ int main()
 #endif
 
 #if !defined(SINGLE_PUSHER) || defined(RK4_ONLY)
-    auto rk = RK4Pusher(ef, mf);
+    auto rk = RK4Pusher(zf, zf);
     rk.setElectronInfo(1, 0, 0, 0, 0, 0);
     const auto rkBegin = std::chrono::steady_clock::now();
     for (int64_t i = 0; i < steps; ++i) {
---
+++ performance-note.md
@@ -0,0 +1,14 @@
+#    Performance Benchmark
+
+##   Intel(R) Core(TM) i7-4700MQ CPU @ 2.40GHz
+###  Win10 VC2017-Release x64
+#### With Gamma Correction
+LeapFrog pusher: 16.9177s
+Boris pusher: 23.4777s
+A-Phi pusher: 10.0256s
+RK4 pusher: 130.429s
+#### Without Gamma Correction
+LeapFrog pusher: 13.9076s
+Boris pusher: 23.4611s
+A-Phi pusher: 7.89667s
+RK4 pusher: 131.201s
