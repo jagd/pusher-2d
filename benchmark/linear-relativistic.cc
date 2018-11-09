487dde31d0543a89a368ad1fa559f5d6d1e3d077
Chuanren Wu <contact@thedrwu.com>
2018-11-09 01:10:50 +0100

    Improve linear relativistic benchmark

--- benchmark/linear-relativistic.cc
+++ benchmark/linear-relativistic.cc
@@ -5,37 +5,46 @@
 
 int main()
 {
-    const double zInit = 0;
     const double r = 1;
-    const double uz0 = 0;
-    const double ur0 = 1e7;
-    const double u0 = std::sqrt(uz0*uz0 + ur0 * ur0);
-    const auto ef = std::make_shared<ConstEzField>(-1000e3/10e-2);
+    const double Ez = -1e8;
+    const auto ef = std::make_shared<ConstEzField>(Ez);
     const auto mf = std::make_shared<ConstBzField>(0);
+
+    auto lf = LeapFrogPusher(ef, mf);
     auto boris = BorisPusher(ef, mf);
-
     auto aphi = APhiPusher(ef, mf);
+    auto rk = RK4Pusher(ef, mf);
 
-    const double dtScale = 0.01;
-    const int64_t steps = static_cast<int64_t>((1 << 22)/dtScale);
-    double dt = 1e-15;
-    boris.setElectronInfo(r, 0, zInit, ur0, 0, uz0);
-    aphi.setElectronInfo(zInit, r, uz0, ur0, aphi.pTheta(zInit, r, 0), u2gamma(u0));
+    const double dtScale = 4096;
+    const int64_t steps = static_cast<int64_t>(1 << 14)/dtScale;
+    const double dt = 1e-15*dtScale;
+    const double vInitHalf = -Ez * Q0/M0*dt / 2 / (std::sqrt(1+(dt*dt/4)*Ez*Ez*Q0*Q0/(M0*M0*C0*C0)));
+    const double uInitHalf = v2u(vInitHalf);
+    lf.setElectronInfo(r, 0, 0, 0, 0, uInitHalf);
+    boris.setElectronInfo(r, 0, 0, 0, 0, uInitHalf);
+    aphi.setElectronInfo(0, r, uInitHalf, 0, aphi.pTheta(0, r, 0), u2gamma(0));
+    rk.setElectronInfo(r, 0, 0, 0, 0, 0);
     std::clog << "dt = " << dt << '\n';
-    for (int i = 0; i < steps; ++i) {
-        aphi.step(dt);
+    int64_t j = 0;
+    for (int64_t i = 1; i <= steps; ++i) {
+        lf.step(dt);
         boris.step(dt);
-        if (i % static_cast<int64_t>(std::ceil(256 / dtScale)) == 0) {
-            std::clog << i * 100.0 / steps << " %\n";
+        aphi.step(dt);
+        rk.step(dt);
+        if (++j >= (128/dtScale)) {
+            j = 0;
+            std::clog << i * 100.0 / steps << " %\n" << aphi.gammaCurrent() << '\n';
             const auto pBoris = boris.pos();
-            const auto pAPhi = aphi.pos();
             const double potEnergyBoris = Q0 * ef->pot(pBoris.z, 0);
             const double kinEnergyBoris = (boris.gammaCurrent() - 1.0)*(M0*C0*C0);
+            const double t = dt * (i);
+            const double exactZ = (M0*C0*C0/Ez/Q0)*(std::sqrt(1+(Ez*Ez*Q0*Q0/(M0*M0*C0*C0)*t*t)) - 1);
             std::cout << i * dt << ' '
-                << boris.pos().x << ' '
+                << exactZ << ' '
+                << lf.pos().z << ' '
                 << boris.pos().z << ' '
-                << aphi.pos().r << ' '
                 << aphi.pos().z << ' '
+                << rk.pos().z << ' '
                 << potEnergyBoris + kinEnergyBoris<< '\n';
         }
     }
