2cd48679b87a724b407c49b0321c8eae48d8f4f8
Chuanren Wu <wuchuanren@gmail.com>
2018-11-21 15:08:18 +0100

    Consider initial uz for linear relativistic benchmark

--- benchmark/linear-relativistic.cc
+++ benchmark/linear-relativistic.cc
@@ -1,12 +1,14 @@
 #include <pusher/pusher.h>
 #include <iostream>
 #include <iomanip>
+#include <cmath>
 
 
 int main()
 {
     const double r = 1;
     const double Ez = -1e8;
+    const double uz0 = -2e8; // free to change
     const auto ef = std::make_shared<ConstEzField>(Ez);
     const auto mf = std::make_shared<ConstBzField>(0);
 
@@ -16,18 +18,17 @@ int main()
     auto rk = RK4Pusher(ef, mf);
 
 #ifdef DEMO
-    const double dtScale = 4096;
+    const double dtScale = 256;
 #else
     for (double dtScale = 4096; dtScale > 100; dtScale /= 1.01) {
 #endif
         const int64_t steps = static_cast<int64_t>((1 << 12) / dtScale);
         const double dt = 1e-15*dtScale;
-        const double vInitHalf = -Ez * Q0 / M0 * dt / 2 / (std::sqrt(1 + (dt*dt / 4)*Ez*Ez*Q0*Q0 / (M0*M0*C0*C0)));
-        const double uInitHalf = v2u(vInitHalf);
+        const double uInitHalf = uz0 - Q0 * Ez / M0 * dt / 2;
         lf.setElectronInfo(r, 0, 0, 0, 0, uInitHalf);
         boris.setElectronInfo(r, 0, 0, 0, 0, uInitHalf);
-        aphi.setElectronInfo(0, r, uInitHalf, 0, aphi.pTheta(0, r, 0), u2gamma(0));
-        rk.setElectronInfo(r, 0, 0, 0, 0, 0);
+        aphi.setElectronInfo(0, r, uInitHalf, 0, aphi.pTheta(0, r, 0), u2gamma(uz0));
+        rk.setElectronInfo(r, 0, 0, 0, 0, uz0);
         std::clog << "dt = " << dt << '\n';
         int64_t j = 0;
         for (int64_t i = 1; i <= steps; ++i) {
@@ -48,7 +49,7 @@ int main()
             const double potEnergyRK = Q0 * ef->pot(rk.pos().z, 0);
             const double kinEnergyRK = (rk.gammaCurrent() - 1.0)*(M0*C0*C0);
             const double t = dt * (i);
-            const double exactZ = (M0*C0*C0 / Ez / Q0)*(std::sqrt(1 + (Ez*Ez*Q0*Q0 / (M0*M0*C0*C0)*t*t)) - 1);
+            const double exactZ = C0 /Q0 /Ez*(std::sqrt(M0*M0*C0*C0 + std::pow(Q0*Ez*t+uz0*M0, 2)) -std::sqrt(M0*M0*C0*C0+uz0*uz0*M0*M0));
             std::cout
                 << dt << ' '
                 << i * dt << ' '
