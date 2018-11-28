cb8746b0dfbde0dba0bdc65ab8a081f5e11ca854
Chuanren Wu <wuchuanren@gmail.com>
2018-11-28 19:22:18 +0100

    Add 2D RK4 to linear Ez test, however, incorrect

--- benchmark/linear-ez.cc
+++ benchmark/linear-ez.cc
@@ -17,7 +17,8 @@ int main()
 
     auto lf = LeapFrogPusher(ef, mf);
     auto boris = BorisPusher(ef, mf);
-    auto aphi = LeapFrogPusher2D(ef, mf);
+    auto lp2d = LeapFrogPusher2D(ef, mf);
+    auto rk2d = RK4Pusher2D(ef, mf);
     auto rk = RK4Pusher(ef, mf);
 
     for (double dtScale = 8192 * 2; dtScale > 0.01; dtScale /= 2) {
@@ -26,21 +27,24 @@ int main()
         const double uInitHalf = uz0 - Q0 * Ez0 / M0 * dt / 2;
         lf.setElectronInfo(r, 0, 0, 0, 0, uInitHalf);
         boris.setElectronInfo(r, 0, 0, 0, 0, uInitHalf);
-        aphi.setElectronInfo(0, r, uInitHalf, 0, aphi.pTheta(0, r, 0), u2gamma(uz0));
+        lp2d.setElectronInfo(0, r, uInitHalf, 0, lp2d.pTheta(0, r, 0), u2gamma(uz0));
+        rk2d.setElectronInfo(0, r, uz0, 0, 0);
         rk.setElectronInfo(r, 0, 0, 0, 0, uz0);
         for (int64_t i = 1; i <= steps; ++i) {
             lf.step(dt);
             boris.step(dt);
-            aphi.step(dt);
+            lp2d.step(dt);
+            rk2d.step(dt);
             rk.step(dt);
         }
 
-        std::clog << "dt = " << dt << " gamma = " << aphi.gammaCurrent() << '\n';
+        std::clog << "dt = " << dt << " gamma = " << lp2d.gammaCurrent() << '\n';
         std::cout
             << dt << ' '
             << lf.pos().z << ' '
             << boris.pos().z << ' '
-            << aphi.pos().z << ' '
+            << lp2d.pos().z << ' '
+            << rk2d.pos().z << ' '
             << rk.pos().z << '\n';
     }
     return 0;
