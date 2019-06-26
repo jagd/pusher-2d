5ef99c59e9c49dd22647266c726711ab84e03ec8
Chuanren Wu <contact@thedrwu.com>
2019-06-26 12:11:44 +0200

    Merge benchmark const-ez and linear-ez, refine solutions

--- benchmark/ezbr.cc
+++ benchmark/ezbr.cc
@@ -1,3 +1,8 @@
+/**
+  This benchmark considers a uniform axial electric field with grad(E) to be a constant (or zero).
+  There can be a magnetic field with a constant Br, which can be zero, too.
+*/
+
 #include <pusher/pusher.h>
 #include <iostream>
 #include <iomanip>
@@ -9,10 +14,11 @@ int main()
 {
     std::cout.precision(std::numeric_limits<double>::max_digits10);
     const double r = 1e9;
-    const double Ez = -1e8;
+    const double Ez0 = -1e8;
+    const double Ek = 0; // free to change, in the test case 1e6
     const double uz0 = -1e8; // free to change
     const double exactETotal = (std::sqrt((uz0*uz0 /C0/C0)+1)-1) *M0*C0*C0 / Q0 * 1e-3;
-    const auto ef = std::make_shared<ConstEzField>(Ez);
+    const auto ef = std::make_shared<LinearEzField>(Ez0, Ek);
     // free to change, Br=0 will yield the exact solution, which is independent of initial velocity
     const auto mf = std::make_shared<ConstBrField>(0);
 
@@ -28,8 +34,8 @@ int main()
 #endif
         const int64_t steps = static_cast<int64_t>((1 << 14) / dtScale);
         const double dt = 1e-15*dtScale;
-        PV3D uInitHalf(0, 0, uz0 - Q0 * Ez / M0 * dt / 2); // if Br = 0
-        if (mf->br(0,r) != 0) {
+        PV3D uInitHalf(0, 0, uz0 - Q0 * Ez0 / M0 * dt / 2); // exact for Br = 0 && Ek == 0
+        if (mf->br(0,r) != 0 || Ek != 0) {
             const int N = 1000000;
             const double reverseSubStep = -dt / 2 / N;
             rk.setElectronInfo(r, 0, 0, 0, 0, uz0);
@@ -40,7 +46,7 @@ int main()
         }
         lf.setElectronInfo(r, 0, 0, uInitHalf.x, uInitHalf.y, uInitHalf.z);
         boris.setElectronInfo(r, 0, 0, uInitHalf.x, uInitHalf.y, uInitHalf.z);
-        aphi.setElectronInfo(0, r, uInitHalf.z, fromPV3D(uInitHalf).r, aphi.pTheta(0, r, 0), u2gamma(uz0));
+        aphi.setElectronInfo(0, r, uInitHalf.z, -fromPV3D(uInitHalf).r, aphi.pTheta(0, r, 0), u2gamma(uz0));
         rk.setElectronInfo(r, 0, 0, 0, 0, uz0);
         std::clog << "dt = " << dt << '\n';
         int64_t j = 0;
@@ -62,7 +68,7 @@ int main()
             const double potEnergyRK = Q0 * ef->pot(rk.pos().z, 0);
             const double kinEnergyRK = (rk.gammaCurrent() - 1.0)*(M0*C0*C0);
             const double t = dt * (i);
-            const double exactZ = C0 /Q0 /Ez*(std::sqrt(M0*M0*C0*C0 + std::pow(Q0*Ez*t+uz0*M0, 2)) -std::sqrt(M0*M0*C0*C0+uz0*uz0*M0*M0));
+            const double exactZ = C0 /Q0 /Ez0*(std::sqrt(M0*M0*C0*C0 + std::pow(Q0*Ez0*t+uz0*M0, 2)) -std::sqrt(M0*M0*C0*C0+uz0*uz0*M0*M0));
             std::cout
                 << dt << ' '
                 << i * dt << ' '
